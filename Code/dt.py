acdfb9d1a4163ce2f2f5e679e524ab3e5344ddea
Sebastian <sebastianbengtsson@me.com>
2024-01-04 17:33:44 +0100

    added dt and knn

---
+++ Code/dt.py
@@ -0,0 +1,35 @@
+import joblib
+import numpy as np
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.utils import shuffle
+from extract_layers import ExctactLayers
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+
+feature_extractor = ExctactLayers('../Models/cnn_V2.h5')
+
+features, labels = feature_extractor.extract()
+
+print('X*X*X*X*X*X*X*X*X*X Training Model X*X*X*X*X*X*X*X*X*X')
+X = np.array(features)
+y = np.array(labels)
+
+X, y = shuffle(X, y, random_state=42)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+print('Done splitting the data')
+
+decision_tree_model = DecisionTreeClassifier()
+
+decision_tree_model.fit(X_train, y_train)
+
+print('Training done, testing model')
+
+y_pred = decision_tree_model.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Accuracy: {accuracy * 100:.2f}%")
+
+model_filename = '../Models/decision_tree.joblib'
+joblib.dump(decision_tree_model, model_filename)
---
+++ Code/knn.py
@@ -0,0 +1,41 @@
+import joblib
+import numpy as np
+from tqdm import tqdm
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.utils import shuffle
+from extract_layers import ExctactLayers
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+
+feature_extractor = ExctactLayers('../Models/cnn_V2.h5')
+
+features, labels = feature_extractor.extract()
+
+print('X*X*X*X*X*X*X*X*X*X Training Model X*X*X*X*X*X*X*X*X*X')
+X = np.array(features)
+y = np.array(labels)
+
+X, y = shuffle(X, y, random_state=42)
+
+print(f'Shapes of the arrays: X={X.shape}, y={y.shape}')
+print(len(X[0]))
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+print('Done splitting the data')
+
+knn_model = KNeighborsClassifier(n_neighbors=5)
+
+print('Fitting the model')
+
+knn_model.fit(X_train, y_train)
+
+print('Training done, testing model')
+
+y_pred = knn_model.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Accuracy: {accuracy * 100:.2f}%")
+
+model_filename = '../Models/knn.joblib'
+joblib.dump(knn_model, model_filename)
