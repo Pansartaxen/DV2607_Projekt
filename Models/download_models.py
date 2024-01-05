d5254c59193ffb2e05fe8aee7bd10123ad3a7eb0
Sebastian <sebastianbengtsson@me.com>
2024-01-05 21:10:33 +0100

    updated svm (svm_v2)

--- Models/download_models.py
+++ Models/download_models.py
@@ -11,6 +11,7 @@ def download_files(file_dict):
 
 file_dict = {
     'cnn_V2.h5': 'https://drive.google.com/uc?id=1IjY7UtTB_EoW4vM4QPObnxRWyC2_VV0-',
+    'svm_model_v2.joblib': 'https://drive.google.com/uc?id=1lNomo0Lestp_SQgFhC9vomM1pI8Vq1w8',
     'svm.joblib': 'https://drive.google.com/uc?id=1yOV9jsYoTabsH382WTasmTC3eMhW9XwX',
     'knn.joblib': 'https://drive.google.com/uc?id=1D34T1vhp3p2MqrMivDQFUvidMXt1mr9S',
     'decision_tree.joblib': 'https://drive.google.com/uc?id=182Q0iwV99kqWYbrVfnneBNA1IJuJbIS8'
